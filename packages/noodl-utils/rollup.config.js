313469ceb340099d4f6d7f011abd50cf76b10b11
Christopher Tran <pfftdammitchris@gmail.com>
2022-07-23 18:54:39 -0700

    init noodl-utils

---
+++ packages/noodl-utils/rollup.config.js
@@ -0,0 +1,40 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import filesize from 'rollup-plugin-filesize'
+import progress from 'rollup-plugin-progress'
+import esbuild from 'rollup-plugin-esbuild'
+
+/**
+ * @type { import('rollup').RollupOptions[] }
+ */
+const configs = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        dir: `dist`,
+        format: 'umd',
+        name: 'nutils',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve({
+        extensions: ['.js'],
+        moduleDirectories: ['node_modules'],
+      }),
+      commonjs(),
+      filesize(),
+      progress(),
+      esbuild({
+        include: /\.ts?$/,
+        exclude: /node_modules/,
+        minify: false,
+        target: 'es2015',
+        sourceMap: true,
+      }),
+    ],
+  },
+]
+
+export default configs
