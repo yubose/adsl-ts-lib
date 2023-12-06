7b26e2418f92148d034c477da70192bba1b15045
Christopher Tran <pfftdammitchris@gmail.com>
2023-12-06 11:30:15 -0800

    updated noodl-test-utils

--- packages/noodl-test-utils/src/index.ts
+++ packages/noodl-test-utils/src/index.ts
@@ -1,6 +1,7 @@
 import actionFactory from './factories/action'
 import builtInFactory from './factories/builtin'
 import componentFactory from './factories/component'
+import otherObjectsFactory from './factories/other'
 import styleFactory from './factories/style'
 
 export * from './types'
@@ -9,6 +10,7 @@ export { actionFactory, componentFactory, styleFactory }
 export default {
   ...actionFactory,
   ...componentFactory,
+  ...otherObjectsFactory,
   builtIn: builtInFactory,
   style: styleFactory,
 }
